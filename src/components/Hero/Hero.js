0a859fe95d1b725195bfa56e0a67c43a7928cd43
Joel-Halkey <98487188+Joel-Halkey@users.noreply.github.com>
2023-11-10 22:42:30 -0500

    Basic hero section structure

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,15 @@ import HeroSectionBGImage from "./HeroSection-bg-image.jpg";
 import "./hero.css";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div className="hero-wrapper">
+      <h1 className="hero-header">Joel's Blog</h1>
+      <h2 className="hero-description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </h2>
+    </div>
+  );
 };
 
 export default Hero;
