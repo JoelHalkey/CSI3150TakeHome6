8c604aa64062388e2f4dc304203bac70d0ddf4f7
Joel-Halkey <98487188+Joel-Halkey@users.noreply.github.com>
2023-11-10 22:34:32 -0500

    TravelBlog basic structure

--- src/components/TravelBlog/TravelBlog.js
+++ src/components/TravelBlog/TravelBlog.js
@@ -1,8 +1,36 @@
 import React from "react";
 import "./travelblog.css";
 
-const TravelBlog = () => {
-  return <div></div>;
+const TravelBlog = (props) => {
+  return (
+    <div className="travelblog-wrapper">
+      <div className="top-row">{props.placeHeading}</div>
+      <div className="middle-row">
+        <div className="place-image-container">
+          <img
+            className="place-image"
+            src={props.placeImg1}
+            alt="Place image 1"
+          />
+        </div>
+        <div className="place-image-container">
+          <img
+            className="place-image"
+            src={props.placeImg2}
+            alt="Place image 2"
+          />
+        </div>
+        <div className="place-image-container">
+          <img
+            className="place-image"
+            src={props.placeImg3}
+            alt="Place image 3"
+          />
+        </div>
+      </div>
+      <div className="bottom-row">{props.placeDescription}</div>
+    </div>
+  );
 };
 
 export default TravelBlog;
