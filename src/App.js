d282fdd2c2f64e5b4945092f0ad26b1efc074768
Joel Halkey <joelhalkey@gmail.com>
2023-11-11 17:47:07 -0500

    comments

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TravelBlog from "./components/TravelBlog/TravelBlog";
 import TravelData from "./TravelData";
 
 function App() {
+  // Loading in the data and pictures from TravelData.js into <TravelBlog> divs
   const travelBlogCards = TravelData.map((item) => {
     return(<TravelBlog
       key={item.id} placeHeading={item.placeHeading} placeImg1={item.placeImg1} placeImg2={item.placeImg2} placeImg3={item.placeImg3} placeDescription={item.placeDescription}
@@ -11,6 +12,7 @@ function App() {
   });
   return <div className="App">
     <Hero/>
+    {/* Displaying the Travelblog divs */}
     <section className='travelblog-card-container'>
         {travelBlogCards}
     </section> 
